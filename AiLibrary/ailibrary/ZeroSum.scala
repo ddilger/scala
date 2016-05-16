5efe967e6500273d0cbfd73e07af307820f45576
Daniel Dilger <ddilger93@gmail.com>
2016-05-16 09:45:30 -0400

    Alpha beta pruning implemented. Works the same as negamax and minimax. Tests needed on how much faster it is.

--- AiLibrary/ailibrary/ZeroSum.scala
+++ AiLibrary/ailibrary/ZeroSum.scala
@@ -48,6 +48,7 @@ object ZeroSum {
     }
   
     /* Minimax */
+  /*
    def minimax_value(startNode: GameNode, maxPlayer: Player, maxTurn: Boolean, depth: Int): Int = {
     val curPlayer = if (maxTurn) {maxPlayer} 
       else {maxPlayer.opponent}
@@ -88,5 +89,76 @@ object ZeroSum {
   def minimax_decision(startNode: GameNode, curPlayer: Player): GameNode = {
     minimax_decision(startNode, curPlayer, 1000)
     }
-  
+  */
+    /* Alpha Beta pruning */
+  def alphabeta_value(startNode: GameNode, maxPlayer: Player, maxTurn: Boolean, alpha: Int, beta: Int, depth: Int): Int = {
+    val curPlayer = if (maxTurn) {maxPlayer} 
+      else {maxPlayer.opponent}
+    val possibleMoves = startNode.nextNodes(curPlayer)
+    //Terminal test
+    if (depth == 0 || possibleMoves.isEmpty) {
+      startNode.value(maxPlayer)
+    }
+    //Max-Value
+    else if (maxTurn){
+      def max_value(a: Int, moves: List[GameNode]): Int = {
+        //println("moves: "+moves)
+        if (moves.isEmpty){
+          a
+        }
+        else{
+            val min_val = alphabeta_value(moves.head, maxPlayer, false, a, beta, depth-1)
+            val new_alpha = if(a > min_val){a} else {min_val}
+            if(new_alpha >= beta){
+              beta
+            }
+            else {
+              
+              max_value(new_alpha, moves.tail)
+            }
+          }
+        }
+      max_value(alpha, possibleMoves)
+    }
+    //Min-Value
+    else {
+       def min_value(b: Int, moves: List[GameNode]): Int = {
+        if (moves.isEmpty){
+          b
+        }
+        else{
+            val max_val = alphabeta_value(moves.head, maxPlayer, true, alpha, b, depth-1)
+            val new_beta = if(b < max_val){b} else {max_val}
+            if(new_beta <= alpha){
+              alpha
+            }
+            else {
+              min_value(new_beta, moves.tail)
+            }
+          }
+        }
+      min_value(beta, possibleMoves)
+    }
+  }
+   //If no depth specified, use an arbitrarily large number 
+   def alphabeta_value(startNode: GameNode, curPlayer: Player): Int = {
+     alphabeta_value(startNode, curPlayer, true, -1000, 1000, 1000)
+   }
+  def alphabeta_decision(startNode: GameNode, curPlayer: Player, depth: Int): GameNode = {
+    val possibleMoves = startNode.nextNodes(curPlayer)
+    if(possibleMoves.isEmpty){
+       null 
+    }
+    else {
+      val valued_choices = possibleMoves.map(n => (n, ZeroSum.alphabeta_value(n, curPlayer, false, -1000, 1000, depth)))
+      //println("valued choices: "+valued_choices)
+      def max(t1:(GameNode, Int), t2:(GameNode, Int)): (GameNode, Int) = if(t1._2 > t2._2) t1 else t2
+      val best_choice = valued_choices.reduceLeft(max) //a (choice, value) pair
+      //println("Best choice value: "+best_choice._2)
+      best_choice._1
+      }
+  }
+  def alphabeta_decision(startNode: GameNode, curPlayer: Player): GameNode = {
+    alphabeta_decision(startNode, curPlayer, 1000)
+    }
 }
